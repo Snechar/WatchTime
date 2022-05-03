e2f65e6bdba2e19cb32be7b79e9c94d2c10d1797
Snechar <Snechboii@gmail.com>
2022-05-03 14:40:36 +0200

    added small video for testing purposes

--- src/video_service/video_service/Controllers/VideoController.cs
+++ src/video_service/video_service/Controllers/VideoController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,35 +20,61 @@ namespace video_service.Controllers
         [HttpGet("{file}")]
         public async Task GetStream(string file)
         {
-            string workingDirectory = Environment.CurrentDirectory;
-            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
-            var provider = new PhysicalFileProvider("H:/XD/Black Desert");
-            string videoPathFile = Path.Combine(workingDirectory, $"Files\\{file}.mp4");
-            byte[] buffer = new byte[1024 * 1024 * 4]; // 'Chunks' of 4MB
-            long startPosition = 0;
-            if (!string.IsNullOrEmpty(Request.Headers["Range"]))
+            try
             {
-                string[] range = Request.Headers["Range"].ToString().Split(new char[] { '=', '-' });
-                startPosition = long.Parse(range[1]);
+                string workingDirectory = Environment.CurrentDirectory;
+                string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+                string videoPathFile = Path.Combine(workingDirectory, $"Files\\{file}.mp4");
+                string[] validExtensions = { ".mp4", ".mov" };
+                if (!System.IO.File.Exists(videoPathFile))
+                {
+                    foreach (var item in validExtensions)
+                    {
+                        videoPathFile = Path.Combine(workingDirectory, $"Files\\{file}{item}");
+                        if (System.IO.File.Exists(videoPathFile))
+                        {
+                            break;
+                        }
+                    }
+                    if (!System.IO.File.Exists(videoPathFile))
+                    {
+                        throw new Exception("File not found");
+                    }
+                }
+                byte[] buffer = new byte[1024 * 1024 * 4]; // 'Chunks' of 4MB
+                long startPosition = 0;
+                if (!string.IsNullOrEmpty(Request.Headers["Range"]))
+                {
+                    string[] range = Request.Headers["Range"].ToString().Split(new char[] { '=', '-' });
+                    startPosition = long.Parse(range[1]);
+                }
+
+                using FileStream inputStream = new(videoPathFile, FileMode.Open, FileAccess.Read, FileShare.Read)
+                {
+                    Position = startPosition
+                };
+                int chunkSize = await inputStream.ReadAsync(buffer.AsMemory(0, buffer.Length));
+                long fileSize = inputStream.Length;
+
+                if (chunkSize > 0)
+                {
+                    Response.StatusCode = 206;
+                    Response.Headers["Accept-Ranges"] = "bytes";
+                    Response.Headers["Content-Range"] = string.Format($" bytes {startPosition}-{fileSize - 1}/{fileSize}");
+                    Response.ContentType = "application/octet-stream";
+
+                    using Stream outputStream = Response.Body;
+                    await outputStream.WriteAsync(buffer.AsMemory(0, chunkSize));
+                };
             }
-
-            using FileStream inputStream = new(videoPathFile, FileMode.Open, FileAccess.Read, FileShare.Read)
+            catch (Exception)
             {
-                Position = startPosition
-            };
-            int chunkSize = await inputStream.ReadAsync(buffer.AsMemory(0, buffer.Length));
-            long fileSize = inputStream.Length;
 
-            if (chunkSize > 0)
-            {
-                Response.StatusCode = 206;
-                Response.Headers["Accept-Ranges"] = "bytes";
-                Response.Headers["Content-Range"] = string.Format($" bytes {startPosition}-{fileSize - 1}/{fileSize}");
-                Response.ContentType = "application/octet-stream";
-
-                using Stream outputStream = Response.Body;
-                await outputStream.WriteAsync(buffer.AsMemory(0, chunkSize));
-            };
+                Response.StatusCode = 500;
+                Response.ContentType = "text/plain";
+                Response.Headers["string"] = "Could not find song";
+            }
+           
         }
         public static DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
         {
